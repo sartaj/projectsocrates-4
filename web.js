cb4203e80019ae580e92085e4c3c4183b85a8c93
Sartaj <sartaj@sartaj.me>
2014-06-10 23:55:17 -0500

    heroku fixes

--- web.js
+++ web.js
@@ -1,7 +1,7 @@
 var gzippo = require('gzippo');
 var express = require('express');
 var logfmt = require("logfmt");
-var app = express.createServer();
+var app = express();
 var port = Number(process.env.PORT || 5000);
 
 process.env.PWD = process.cwd()
