708c98a7263acc061c036c9f70c7c418fd33369e
Sartaj <sartaj@sartaj.me>
2014-06-10 22:38:30 -0500

    heroku fixes

--- web.js
+++ web.js
@@ -1,21 +1,19 @@
 process.env.PWD = process.cwd()
 
-
 var gzippo = require('gzippo');
 var express = require('express');
 var logfmt = require("logfmt");
 var app = express();
 
-// app.use(logfmt.requestLogger());
-
 var homeUrl = process.env.PWD + "/client-web/0-2/dist/"; 
-
-console.log('Directory: ', homeUrl);
-
-app.use(gzippo.staticGzip(homeUrl));
-
 var port = Number(process.env.PORT || 5000);
 
-app.listen(port, function() {
-  console.log("Listening on " + port);
-}); 
+// app.use(logfmt.requestLogger());
+
+app.get('/', function(request, response) {
+    response.sendfile(homeUrl+ 'index.html');
+}).configure(function() {
+	app.use(gzippo.staticGzip(homeUrl));
+}).listen(port, function(){
+    console.log("Listening on " + port);
+});
