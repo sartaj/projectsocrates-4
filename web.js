a242929379c3c399feec3ebfd7925c215f3c02ee
Sartaj <sartaj@sartaj.me>
2014-06-11 00:16:52 -0500

    heroku fixes

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express();
 var port = Number(process.env.PORT || 5000);
 
 process.env.PWD = process.cwd()
-var homeUrl = "/client-web/0-2/dist"; 
+var homeUrl = process.env.PWD + "/public"; 
 
 app.use(logfmt.requestLogger());
 
