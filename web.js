346447013f5e984f5938a154d3ce0e1d7f60382d
Sartaj <sartaj@sartaj.me>
2014-06-11 00:45:55 -0500

    heroku workings

--- web.js
+++ web.js
@@ -4,7 +4,7 @@ var logfmt = require("logfmt");
 var app = express();
 
 process.env.PWD = process.cwd()
-var homeUrl = process.env.PWD + "client-web/0-2/dist"; 
+var homeUrl = process.env.PWD + "client-web/0-2/dist";
 
 app.use(logfmt.requestLogger());
 
@@ -12,8 +12,9 @@ app.use(gzippo.staticGzip(homeUrl));
 
 var port = Number(process.env.PORT || 5000);
 
-app.listen(port, function() {
-  console.log("Listening on " + port);
-}); 
+app.listen(port, function () {
+    console.log("HOME URL: ", homeUrl);
+    console.log("Listening on " + port);
+});
 
 //
